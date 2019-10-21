18d6061da21ba1ee429453a879c833d0b368c22b
Jason Quinn <jquinn@gmail.com>
2019-10-21 09:12:18 -0500

    Add oath component

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://jason123:1F74i9oEsWLB@cluster0-boqhj.mongodb.net/test?retryWrites=true&w=majority',
+  googleClientID:
+    '930719092061-de1tlu3c6d73tfn08h4unk6r1pb3h0kj.apps.googleusercontent.com',
+  googleClientSecret: 'PPG3R_fzIBiUEboqUSWQ7cFm'
+};
