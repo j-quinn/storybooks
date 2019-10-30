14ee94bc8d35636be73867d1ec87822fcd4f3692
Jason Quinn <jquinn@gmail.com>
2019-10-30 09:50:30 -0500

    version 1.0.0alpha

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 
-// Load User Model
+// Load Models
 require('./models/User');
+require('./models/Story');
 
 // Passport config
 require('./config/passport')(passport);
@@ -13,9 +17,19 @@ require('./config/passport')(passport);
 // Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 // Load Keys
 const keys = require('./config/keys');
+
+// Handlebars Helpers
+const {
+  truncate,
+  stripTags,
+  formatDate,
+  select,
+  editIcon
+} = require('./helpers/hbs');
 
 // Map global promises
 mongoose.Promise = global.Promise;
@@ -30,10 +44,24 @@ mongoose
 
 const app = express();
 
+// shouldn't be needed w/ handlebars version
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Method Override Middleware
+app.use(methodOverride('_method'));
+
 // Handlebars middleware
 app.engine(
   'handlebars',
   exphbs({
+    helpers: {
+      truncate: truncate,
+      stripTags: stripTags,
+      formatDate: formatDate,
+      select: select,
+      editIcon: editIcon
+    },
     defaultLayout: 'main'
   })
 );
@@ -58,9 +86,13 @@ app.use((req, res, next) => {
   next();
 });
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Use Routes
 app.use('/', index);
 app.use('/auth', auth);
+app.use('/stories', stories);
 
 const port = process.env.PORT || 5000;
 
--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://jason123:1F74i9oEsWLB@cluster0-boqhj.mongodb.net/test?retryWrites=true&w=majority',
-  googleClientID:
-    '930719092061-de1tlu3c6d73tfn08h4unk6r1pb3h0kj.apps.googleusercontent.com',
-  googleClientSecret: 'PPG3R_fzIBiUEboqUSWQ7cFm'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('keys_prod.js');
+} else {
+  module.exports = require('keys_dev.js');
+}
